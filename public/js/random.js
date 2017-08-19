5f1fb97527ac494fd5d06bc463a60cdfc1b29fe7
Manan <jn.manan25@gmail.com>
2017-08-19 22:04:03 +0530

    Randomized lanes of opposite cars

--- public/js/random.js
+++ public/js/random.js
@@ -1,25 +1,27 @@
-var scene = document.querySelector('a-scene');
-var cylinder = document.createElement('a-cylinder');
-cylinder.setAttribute('color', '#FF9500');
-cylinder.setAttribute('height', '2');
-cylinder.setAttribute('radius', '0.75');
-
+// var scene = document.querySelector('a-scene');
+// var box = document.createElement('a-box');
+// box.setAttribute('rotation', '0 90 0');
+// box.setAttribute('scale','3 3 3');
+var i=0;
+for(i=0;i<10;i++)
+{
+	setTimeout(function () {}, 3000);
 var lane = Math.floor(Math.random() * 19);
 lane=lane%3;
 
-var x=0;
-
+var c=0;
 
 if(lane==0)
-{
-cylinder.setAttribute('position', '3 1 10' );
-}
+{c=5;}
 else if(lane==1)
-{
-cylinder.setAttribute('position', '3 1 0' );
-}
+{c=0;}
 else
-{
-cylinder.setAttribute('position', '3 1 -10' );
+{c=-5;}
+
+
+// scene.appendChild(box);
+function GetLane(){
+return c;
+	}
+
 }
-scene.appendChild(cylinder);
