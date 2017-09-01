12673241730d96a157c6a6b7cd07d20869cd99ad
kartik-25 <pandeykartikey99@gmail.com>
2017-09-02 01:03:27 +0530

    Basic Scoring Completed

--- public/js/collision.js
+++ public/js/collision.js
@@ -1,6 +1,7 @@
 var playerEl = document.getElementById('fixed-car');
 playerEl.addEventListener('collide', function (e) {
   console.log('Player has collided with body #' + e.detail.body.id);
+  console.log(e);
   if(e.detail.body.id == 1){
 	alert("You Lost");
   }
--- public/js/score.js
+++ public/js/score.js
@@ -1,14 +1,6 @@
 localStorage.setItem('score_vr_race', 0);
-    //setInterval(function(){
-//document.getElementById('moving-car').addEventListener('alongpath-curve1-activated',
-setInterval(
-	function(){
-coord_moving = document.getElementById('moving-car').getAttribute('position');
-coord_fixed = document.getElementById('fixed-car').getAttribute('position');
-//if(coord_fixed[0] === coord_moving[0]){
-//	localStorage.setItem('score_vr_race',localStorage.getItem('score_vr_race')+1);
-//	console.log("here");
-//	}
-console.log(coord_fixed.x);
-console.log(coord_moving.x);
-},1);
+
+document.getElementById('curve2').addEventListener('alongpath-trigger-activated', function() {
+    localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
+    console.log(localStorage.getItem('score_vr_race'));
+});
