c3b1d0fa07e66cdeeb5d0eb18af128c60ebfa2f1
kartik-25 <pandeykartikey99@gmail.com>
2017-09-02 03:34:26 +0530

    Removed physics library dependency

--- public/js/collision.js
+++
@@ -1,9 +0,0 @@
-var playerEl = document.getElementById('fixed-car');
-playerEl.addEventListener('collide', function (e) {
-  console.log('Player has collided with body #' + e.detail.body.id);
-  console.log(e);
-  if(e.detail.body.id == 1){
-	alert("You Lost");
-  }
-
-});
--- public/js/score.js
+++ public/js/score.js
@@ -1,6 +1,13 @@
 localStorage.setItem('score_vr_race', 0);
 
 document.getElementById('curve2').addEventListener('alongpath-trigger-activated', function() {
+	coord_moving = document.getElementById('moving-car').getAttribute('position');
+    coord_fixed = document.getElementById('fixed-car').getAttribute('position');
+    console.log(coord_fixed);
+    console.log(coord_moving);
+    if(coord_fixed.z === coord_moving.z){
+		alert('You Lost')
+    }
     localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
     console.log(localStorage.getItem('score_vr_race'));
 });
