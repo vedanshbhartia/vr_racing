aa88da170c78caf54f82cbba9f02d6876e0d0094
Vedansh Bhartia <vedanshbhartia@gmail.com>
2017-09-03 00:03:56 +0530

    Changed collision event to a z-axis range instead of absolute equality

--- public/js/score.js
+++ public/js/score.js
@@ -5,7 +5,7 @@ document.getElementById('curve2').addEventListener('alongpath-trigger-activated'
     coord_fixed = document.getElementById('fixed-car').getAttribute('position');
     console.log(coord_fixed);
     console.log(coord_moving);
-    if(coord_fixed.z === coord_moving.z){
+    if(coord_fixed.z >= coord_moving.z -3 && coord_fixed<= coord_moving + 3){
 		alert('You Lost')
     }
     localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
