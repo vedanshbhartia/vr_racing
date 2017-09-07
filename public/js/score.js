bb09bbc627452d66f50e7cae0a189abe689f620d
kartik-25 <pandeykartikey99@gmail.com>
2017-09-08 00:13:33 +0530

    Added score display

--- public/js/score.js
+++ public/js/score.js
@@ -3,11 +3,18 @@ localStorage.setItem('score_vr_race', 0);
 document.getElementById('curve2').addEventListener('alongpath-trigger-activated', function() {
 	coord_moving = document.getElementById('moving-car').getAttribute('position');
     coord_fixed = document.getElementById('fixed-car').getAttribute('position');
-    console.log(coord_fixed);
-    console.log(coord_moving);
-    if(coord_fixed.z >= coord_moving.z -3 && coord_fixed<= coord_moving + 3){
-		alert('You Lost')
+    if(coord_fixed.z >= coord_moving.z -3 && coord_fixed.z<= coord_moving.z + 3){
+			
+    console.log("moving"+JSON.stringify(coord_moving));
+	    console.log("fixed"+JSON.stringify(coord_fixed));
+	    alert('You Lost! Your Score is '+localStorage.getItem('score_vr_race'));
+	    localStorage.setItem('score_vr_race', 0);
+		return;
     }
     localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
-    console.log(localStorage.getItem('score_vr_race'));
+    var scoredisplay=document.getElementById("score_overlay");
+console.log(scoredisplay);
+    scoredisplay.setAttribute("text","align: center; width: 100;color:red; height:100; value:"+localStorage.getItem('score_vr_race')+";");
+	console.log(scoredisplay);
+    	console.log(localStorage.getItem('score_vr_race'));
 });
