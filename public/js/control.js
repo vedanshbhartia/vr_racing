442ec71324d4107f6bc77d9b093700c90a828aa9
kartik-25 <pandeykartikey99@gmail.com>
2017-09-08 02:26:36 +0530

    Made collisions better

--- public/js/control.js
+++ public/js/control.js
@@ -1,18 +1,18 @@
 var car = document.querySelector('.control-this');
 
 function handleOrientation(event) {
-    var alpha    = event.beta;
-    
-    if (alpha >  90) {alpha =  90};
-    if (alpha < -90) {alpha = -90};
+    var alpha = event.beta;
 
-    const maxwidth=55;
+    if (alpha > 90) { alpha = 90 };
+    if (alpha < -90) { alpha = -90 };
 
-    var new_z = (maxwidth*(alpha/180));
-    var temp_pos = "0 0 "+new_z;
+    const maxwidth = 30;
+
+    var new_z = (maxwidth * (alpha / 180));
+    var temp_pos = "0 0 " + new_z;
 
     document.getElementsByClassName('control-this')[0].setAttribute("position", temp_pos);
-    
-    }
+
+}
 
 window.addEventListener('deviceorientation', handleOrientation);
--- public/js/score.js
+++ public/js/score.js
@@ -1,20 +1,34 @@
 localStorage.setItem('score_vr_race', 0);
 
 document.getElementById('curve2').addEventListener('alongpath-trigger-activated', function() {
-	coord_moving = document.getElementById('moving-car').getAttribute('position');
-    coord_fixed = document.getElementById('fixed-car').getAttribute('position');
-    if(coord_fixed.z >= coord_moving.z -3 && coord_fixed.z<= coord_moving.z + 3){
-			
-    console.log("moving"+JSON.stringify(coord_moving));
-	    console.log("fixed"+JSON.stringify(coord_fixed));
-	    alert('You Lost! Your Score is '+localStorage.getItem('score_vr_race'));
-	    localStorage.setItem('score_vr_race', 0);
-		return;
-    }
+    let collision = false;
+    let timerId = setInterval(function() {
+        coord_moving = document.getElementById('moving-car').getAttribute('position');
+        coord_fixed = document.getElementById('fixed-car').getAttribute('position');
+        if (coord_fixed.z >= coord_moving.z - 3 && coord_fixed.z <= coord_moving.z + 3) {
+            console.log("moving" + JSON.stringify(coord_moving));
+            console.log("fixed" + JSON.stringify(coord_fixed));
+            var score = localStorage.getItem('score_vr_race');
+            localStorage.setItem('score_vr_race', 0);
+            collision = true;
+            alert('You Lost! Your Score is ' + score);
+            clearInterval(timerId);
+        }
+    }, 1);
+    document.getElementById('curve2').addEventListener('alongpath-trigger-deactivated', function() {
+        clearInterval(timerId);
+        if(collision){
+            collision = false;
+            localStorage.setItem('score_vr_race', 0);
+            var scoredisplay = document.getElementById("score_overlay");
+            scoredisplay.setAttribute("text", "align: center; width: 100;color:red; height:100; value:" + localStorage.getItem('score_vr_race') + ";");    
+        }
+    });
+});
+
+document.getElementById('curve2').addEventListener('alongpath-trigger-deactivated', function() {
     localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
-    var scoredisplay=document.getElementById("score_overlay");
-console.log(scoredisplay);
-    scoredisplay.setAttribute("text","align: center; width: 100;color:red; height:100; value:"+localStorage.getItem('score_vr_race')+";");
-	console.log(scoredisplay);
-    	console.log(localStorage.getItem('score_vr_race'));
+    var scoredisplay = document.getElementById("score_overlay");
+    scoredisplay.setAttribute("text", "align: center; width: 100;color:red; height:100; value:" + localStorage.getItem('score_vr_race') + ";");
+    console.log(localStorage.getItem('score_vr_race'));
 });
