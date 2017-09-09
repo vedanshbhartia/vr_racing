e604a2a2eaa0c05613d0de10461eaab094a7aa9a
kartik-25 <pandeykartikey99@gmail.com>
2017-09-09 14:07:38 +0530

    Added Coins for bonus score

--- public/js/score.js
+++ public/js/score.js
@@ -1,34 +1,69 @@
 localStorage.setItem('score_vr_race', 0);
-let collision = false;
-let timerId = 0;
+let collisionCar = false;
+let collisionCoin = false;
+let timerIdCar = 0;
+let timerIdCoin = 0;
+
+function checkCollision(z1, z2) {
+    if (z1 >= z2 - 3 && z1 <= z2 + 3) {
+        return true;
+    }
+
+    return false;
+}
+
+function updateDisplayScore() {
+    var scoredisplay = document.getElementById("score_overlay");
+    scoredisplay.setAttribute("text", "align: center; width: 100;color:red; height:100; value:" + localStorage.getItem('score_vr_race') + ";");
+}
 
 document.getElementById('curve2').addEventListener('alongpath-trigger-activated', function() {
-    collision = false;
-    timerId = setInterval(function() {
+    collisionCar = false;
+    timerIdCar = setInterval(function() {
         coord_moving = document.getElementById('moving-car').getAttribute('position');
         coord_fixed = document.getElementById('fixed-car').getAttribute('position');
-        if (coord_fixed.z >= coord_moving.z - 3 && coord_fixed.z <= coord_moving.z + 3) {
+        collisionCar = checkCollision(coord_fixed.z, coord_moving.z)
+        if (collisionCar) {
             var score = localStorage.getItem('score_vr_race');
             localStorage.setItem('score_vr_race', 0);
-            collision = true;
             alert('You Lost! Your Score is ' + score);
-            clearInterval(timerId);
-            interval = 5000;
+            starttime = updatetime;
+            clearInterval(timerIdCar);
         }
     }, 1);
 });
 
 document.getElementById('curve2').addEventListener('alongpath-trigger-deactivated', function() {
-    clearInterval(timerId);
-    if (collision) {
-        collision = false;
-        localStorage.setItem('score_vr_race', 0);
-        var scoredisplay = document.getElementById("score_overlay");
-        scoredisplay.setAttribute("text", "align: center; width: 100;color:red; height:100; value:" + localStorage.getItem('score_vr_race') + ";");
+    clearInterval(timerIdCar);
+    if (collisionCar) {
+        collisionCar = false;
+    } else {
+        localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
+    }
+    updateDisplayScore();
+    console.log(localStorage.getItem('score_vr_race'));
+});
+
+document.getElementById('coin2').addEventListener('alongpath-trigger-activated', function() {
+    collisionCoin = false;
+    timerIdCoin = setInterval(function() {
+        coord_fixed = document.getElementById('fixed-car').getAttribute('position');
+        coord_coin = document.getElementById('coin').getAttribute('position');
+        collisionCoin = checkCollision(coord_fixed.z, coord_coin.z)
+        if (collisionCoin) {
+            var score = localStorage.getItem('score_vr_race');
+            localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 5);
+            clearInterval(timerIdCoin);
+        }
+    }, 1);
+});
+
+document.getElementById('coin2').addEventListener('alongpath-trigger-deactivated', function() {
+    clearInterval(timerIdCoin);
+    if (collisionCoin) {
+        collisionCoin = false;
+        updateDisplayScore();
         return;
     }
-    localStorage.setItem('score_vr_race', parseInt(localStorage.getItem('score_vr_race')) + 1);
-    var scoredisplay = document.getElementById("score_overlay");
-    scoredisplay.setAttribute("text", "align: center; width: 100;color:red; height:100; value:" + localStorage.getItem('score_vr_race') + ";");
     console.log(localStorage.getItem('score_vr_race'));
 });
